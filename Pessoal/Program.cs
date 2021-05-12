91154198bd0ac5c45cc7901cd3c4dc25172d1e02
Yuri Lucas <ylcurti2@gmail.com>
2021-05-11 21:07:24 -0300

    Primeiro

---
+++ Pessoal/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pessoal {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
+        }
+    }
+}
